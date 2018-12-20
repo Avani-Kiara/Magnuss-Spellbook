8c5dccc8149a3442237b7e1338a6b55cfe68c195
Avani-Kiara <43829714+Avani-Kiara@users.noreply.github.com>
2018-12-19 22:07:21 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".Disaperium").click(function() {
+    $(".disappear").fadeToggle();   
+});
+
+$(".Angelirium").click(function() {
+    $(".ethereal").toggle();
+});
+  
+$(".protectorous").click(function() {
+    $(".protector").slideToggle();
+});
